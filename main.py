b32b6381cf4ba1f6627705ddb90bced8d0431509
hdyard <hdyard@gmail.com>
2021-01-20 22:48:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,12 +55,13 @@ if __name__ == '__main__':
     cookies = os.environ["COOKIES"]
     sb.load_cookie_str(cookies)
     res = sb.checkin()
+    msg = res[6]
     print(res)
     SERVERCHAN_SECRETKEY = os.environ["SERVERCHAN_SECRETKEY"]
     print('sc_key: ', SERVERCHAN_SECRETKEY)
     if isinstance(SERVERCHAN_SECRETKEY,str) and len(SERVERCHAN_SECRETKEY)>0:
         print('检测到 SCKEY， 准备推送')
-        push_to_wechat(text = '什么值得买每日签到'+res,
+        push_to_wechat(text = '什么值得买每日签到'+str(msg),
                         desp = str(res),
                         secretKey = SERVERCHAN_SECRETKEY)
     print('代码完毕')
